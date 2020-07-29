0976125ffe5680a7037794d74abf3733a9af789b
JR Maschietto <maschietto.jr@gmail.com>
2020-07-29 19:23:06 -0400

    Google Sign In feedback

--- js/index.js
+++ js/index.js
@@ -75,4 +75,6 @@
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  alert( profile.getName() + ', Logged In' )
 }
